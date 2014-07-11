aed85eb48acd3e31ce61997b184cdfcacb8ab279
gworsham <gworsham@2bba9189-f148-4ba5-8c23-5536c8a10965>
2014-07-11 05:06:43 +0000

    Added LOGFREQ option to SliderLabel, trying to get it working.   When slider moves, it shows log of freq rather than freq. Also not sure that freq parameter is getting set correctly in code generation.

--- Java/SpinCADBuilder/trunk/com.holycityaudio.spincadmenu/xtend-gen/com/holycityaudio/spincadmenu/generator/SpinCADMenuGenerator.java
+++ Java/SpinCADBuilder/trunk/com.holycityaudio.spincadmenu/xtend-gen/com/holycityaudio/spincadmenu/generator/SpinCADMenuGenerator.java
@@ -50,35 +50,288 @@ public class SpinCADMenuGenerator implements IGenerator {
     {
       EList<Element> _elements = mn.getElements();
       for(final Element m : _elements) {
-        CharSequence _switchResult = null;
         boolean _matched = false;
         if (!_matched) {
           if (m instanceof TEST_ITEM) {
-            final TEST_ITEM _tEST_ITEM = (TEST_ITEM)m;
             _matched=true;
-            CharSequence _genTestItem = this.genTestItem(_tEST_ITEM, resource, fsa);
-            _switchResult = _genTestItem;
+            this.genTestItem(((TEST_ITEM)m), resource, fsa);
           }
         }
-        _builder.append(_switchResult, "");
         _builder.newLineIfNotEmpty();
       }
     }
     return _builder;
   }
   
-  public CharSequence genTestItem(final TEST_ITEM m, final Resource resource, final IFileSystemAccess fsa) {
-    CharSequence _xblockexpression = null;
-    {
-      String pkage = "\\com\\holycityaudio\\SpinCAD\\test\\";
-      StringConcatenation _builder = new StringConcatenation();
-      _xblockexpression = (_builder);
-    }
-    return _xblockexpression;
+  public void genTestItem(final TEST_ITEM m, final Resource resource, final IFileSystemAccess fsa) {
+    String pkage = "\\com\\holycityaudio\\SpinCAD\\test\\";
   }
   
-  public Object toTestCode(final Menu mn) {
-    return null;
+  public CharSequence toTestCode(final Menu mn) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("/* SpinCAD Designer - DSP Development Tool for the Spin FV-1");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("* Copyright (C) 2013 - 2014 - Gary Worsham");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("* Based on ElmGen by Andrew Kilpatrick.  Modified by Gary Worsham 2013 - 2014.  Look for GSW in code.");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("* ");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("*   This program is free software: you can redistribute it and/or modify");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("*   it under the terms of the GNU General Public License as published by");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("*   the Free Software Foundation, either version 3 of the License, or");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("*   (at your option) any later version.");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("*");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("*   This program is distributed in the hope that it will be useful,");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("*   but WITHOUT ANY WARRANTY; without even the implied warranty of");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("*   GNU General Public License for more details.");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("*");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("*   You should have received a copy of the GNU General Public License");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("*   along with this program.  If not, see <http://www.gnu.org/licenses/>.");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("* \t");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("*/");
+    _builder.newLine();
+    _builder.newLine();
+    _builder.append("// this is a bunch of units tests for the blocks");
+    _builder.newLine();
+    _builder.append("// could stand improvement");
+    _builder.newLine();
+    _builder.newLine();
+    _builder.append("package com.holycityaudio.SpinCAD.test;");
+    _builder.newLine();
+    _builder.newLine();
+    _builder.append("import javax.swing.SwingUtilities;");
+    _builder.newLine();
+    _builder.newLine();
+    _builder.append("import com.holycityaudio.SpinCAD.SpinCADFrame;");
+    _builder.newLine();
+    _builder.newLine();
+    _builder.append("import org.andrewkilpatrick.elmGen.ElmProgram;");
+    _builder.newLine();
+    _builder.append("import org.andrewkilpatrick.elmGen.simulator.SimulatorState;");
+    _builder.newLine();
+    _builder.append("import org.andrewkilpatrick.elmGen.simulator.SinLFO;");
+    _builder.newLine();
+    _builder.newLine();
+    _builder.append("public class SpinCADTest {");
+    _builder.newLine();
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("public SpinCADTest() {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("// ------------------------------------------------------------");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("/**");
+    _builder.newLine();
+    _builder.append("\t ");
+    _builder.append("* Launch the application.");
+    _builder.newLine();
+    _builder.append("\t ");
+    _builder.append("*/");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("public static void main(String[] args) {");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.append("final ElmProgram p = new ElmProgram(\"Test\");");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.append("SwingUtilities.invokeLater(new Runnable() {");
+    _builder.newLine();
+    _builder.append("\t\t\t");
+    _builder.append("public void run() {");
+    _builder.newLine();
+    _builder.append("\t\t\t\t");
+    _builder.append("try { ");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("SpinCADFrame dspFrame = new SpinCADFrame();");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("dspFrame.setVisible(true);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("// these were generated by GenTestCases() - old Java written CADBlocks");
+    _builder.newLine();
+    _builder.append("/*\t\t\t\t\tnew BitCrusherTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new SingleDelayTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new MultiTapTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new HPF2PTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new OneBandEQTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new SixBandEQTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new BiQuadTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new ChorusTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new ChorusPresetTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new DelayTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new LPF4PTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("//\t\t\t\t\tnew CubeGainTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("*/\t\t\t\t\t");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("// these are auto-generated from spincad source files");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new very_tight_plate_verbTest(dspFrame);\t");
+    _builder.newLine();
+    _builder.append("//\t\t\t\t\tnew InstructionTestTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("//\t\t\t\t\tnew dance_ir_fla_lTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new ToverXTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new OctaveTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new distTest(dspFrame);\t");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new ga_demo_chorusTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new ga_demo_echoTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new ga_demo_flangerTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new crossfadeTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new control_smootherTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new ga_demo_flangerTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new MN3011aTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new ga_demo_phaserTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new ga_demo_vibratoTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new ga_demo_wahTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new rms_lim_expTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new rms_limiterTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new slow_gearTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new gated_verbTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new rom_rev1Test(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new shimmer_verbTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new act_xoverTest(dspFrame); \t");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("new soft_knee_limiterTest(dspFrame);");
+    _builder.newLine();
+    _builder.append("\t\t\t\t");
+    _builder.append("} catch (Exception e) {");
+    _builder.newLine();
+    _builder.append("\t\t\t\t\t");
+    _builder.append("e.printStackTrace();");
+    _builder.newLine();
+    _builder.append("\t\t\t\t");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("\t\t\t");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.append("});");
+    _builder.newLine();
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("}");
+    _builder.newLine();
+    return _builder;
   }
   
   public CharSequence toMenuCode(final Menu mn) {
@@ -100,13 +353,12 @@ public class SpinCADMenuGenerator implements IGenerator {
           boolean _matched = false;
           if (!_matched) {
             if (m instanceof MENU_ITEM) {
-              final MENU_ITEM _mENU_ITEM = (MENU_ITEM)m;
               _matched=true;
-              CharSequence _importMenuItem = this.importMenuItem(_mENU_ITEM);
+              CharSequence _importMenuItem = this.importMenuItem(((MENU_ITEM)m));
               _switchResult = _importMenuItem;
             }
           }
-          _builder.append(_switchResult, "	");
+          _builder.append(_switchResult, "\t");
           _builder.newLineIfNotEmpty();
         }
       }
@@ -133,7 +385,7 @@ public class SpinCADMenuGenerator implements IGenerator {
       Resource _eResource = mn.eResource();
       String _className = this.className(_eResource);
       String _plus = (_className + "Menu");
-      _builder.append(_plus, "	");
+      _builder.append(_plus, "\t");
       _builder.append(" {");
       _builder.newLineIfNotEmpty();
       _builder.newLine();
@@ -145,7 +397,7 @@ public class SpinCADMenuGenerator implements IGenerator {
       Resource _eResource_1 = mn.eResource();
       String _className_1 = this.className(_eResource_1);
       String _plus_1 = (_className_1 + "Menu");
-      _builder.append(_plus_1, "	");
+      _builder.append(_plus_1, "\t");
       _builder.append("(final SpinCADFrame f, final SpinCADPanel panel, JMenuBar menuBar) {");
       _builder.newLineIfNotEmpty();
       _builder.newLine();
@@ -157,14 +409,13 @@ public class SpinCADMenuGenerator implements IGenerator {
           boolean _matched_1 = false;
           if (!_matched_1) {
             if (m_1 instanceof MENU_HEADER) {
-              final MENU_HEADER _mENU_HEADER = (MENU_HEADER)m_1;
               _matched_1=true;
               String _xblockexpression_1 = null;
               {
-                String _name = _mENU_HEADER.getName();
+                String _name = ((MENU_HEADER)m_1).getName();
                 String _menuName = this.getMenuName(_name);
                 menuNameY = _menuName;
-                String _genMenu = this.genMenu(_mENU_HEADER);
+                String _genMenu = this.genMenu(((MENU_HEADER)m_1));
                 _xblockexpression_1 = (_genMenu);
               }
               _switchResult_1 = _xblockexpression_1;
@@ -172,13 +423,12 @@ public class SpinCADMenuGenerator implements IGenerator {
           }
           if (!_matched_1) {
             if (m_1 instanceof MENU_ITEM) {
-              final MENU_ITEM _mENU_ITEM = (MENU_ITEM)m_1;
               _matched_1=true;
-              CharSequence _genMenuItem = this.genMenuItem(_mENU_ITEM, menuNameY);
+              CharSequence _genMenuItem = this.genMenuItem(((MENU_ITEM)m_1), menuNameY);
               _switchResult_1 = _genMenuItem;
             }
           }
-          _builder.append(_switchResult_1, "	");
+          _builder.append(_switchResult_1, "\t");
           _builder.newLineIfNotEmpty();
         }
       }
@@ -236,7 +486,7 @@ public class SpinCADMenuGenerator implements IGenerator {
     _builder.append("\t\t");
     _builder.append("SpinCADBlock pcB = new ");
     String _className_2 = m.getClassName();
-    _builder.append(_className_2, "		");
+    _builder.append(_className_2, "\t\t");
     _builder.append("CADBlock(50, 100);");
     _builder.newLineIfNotEmpty();
     _builder.append("\t\t");
